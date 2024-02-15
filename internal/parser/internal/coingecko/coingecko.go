cf2d641631cfc1d8ab162fc136c537bdd537800e
emidev98 <emilian@terra.money>
2024-02-15 17:56:10 +0200

    feat: refact osmo to pull individual pools

--- internal/parser/internal/coingecko/coingecko.go
+++ internal/parser/internal/coingecko/coingecko.go
@@ -28,6 +28,7 @@ var COIN_GECKO_MAPPING = map[string]string{
 	"white-whale":        "WHALE",  // White Whale chain
 	"switcheo":           "SWTH",   // Carbon chain
 	"stride-staked-luna": "STLUNA", // Stride chain
+	"osmosis":            "OSMO",
 }
 
 func ParseSymbol(symbol string) (string, string, error) {
